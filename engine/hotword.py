3f415ab6424a3da67f417fb972b509c1901eba45
Fipsi <scheer28philipp@gmail.com>
2020-08-01 11:48:47 +0200

    add all files from raspberry pi installation

--- engine/hotword.py
+++ engine/hotword.py
@@ -9,49 +9,61 @@
 ## output: jarvis/hotword -> (detected|started|stopped|error)
 
 
+MODEL_PATH = "/home/pi/jarvis/resources/hotword/alexa.hotword"
+SENSITIVITY = 0.2					## 0.6 - a lot of false positives			## 0.05 - works good...
+
+
 import lib.helper as helper
-import sys
+import lib.snowboy.examples.Python3.snowboydecoder as snowboy
+import signal, os, sys, time
 
 
 mqtt = helper.MQTT(client_id="hotword")
-
-
-if sys.version_info[0] != 2:
-	mqtt.publish("jarvis/hotword", "error")
-	helper.log("hotw", "hotword.py must be run with python 2")
-	sys.exit(-1)
-
-
-import lib.snowboy.snowboydetect as snowboydetect
-import lib.snowboy.snowboydecoder as snowboy
-import signal
+lights = helper.Lights()
 
 
 interrupted = False
 
 
 def detected_callback():
+	global interrupted, mqtt, lights
 	mqtt.publish("jarvis/hotword", "detected")
+	lights.add_color("newcolor", [ 255, 200, 60 ])
+	lights.set( [ "yellow", 0, 0, "red", 0, 0, "green", 0, 0, "blue", 0, 0 ] )
+
+	lights.on()
+	i = 0
+	while i < 20:
+		if interrupted:
+			exit(0)
+		else:
+			lights.rotate()
+			time.sleep(0.06)
+			i += 1
+	lights.off()
 
 
 def signal_handler(signal, frame):
-	global interrupted
+	global interrupted, lights, mqtt
 	interrupted = True
+	mqtt.publish("jarvis/hotword", "stopped")
+	lights.off()
+	exit(0)
+	
 
 def interrupt_callback():
 	global interrupted
 	return interrupted
 
 
-if len(sys.argv) == 1:
+if not os.path.isfile(MODEL_PATH): 
 	mqtt.publish("jarvis/hotword", "error")
-	helper.log("hotw", "no model name specified, exiting")
+	helper.log("hotw", "model doesn't exist at {}".format(MODEL_PATH))
 	sys.exit(-1)
 
 
-model = sys.argv[1]
 signal.signal(signal.SIGINT, signal_handler)
-detector = snowboy.HotwordDetector(model, sensitivity=0.5)
+detector = snowboy.HotwordDetector(MODEL_PATH, sensitivity=SENSITIVITY)
 mqtt.publish("jarvis/hotword", "started")
 
 
@@ -61,4 +73,3 @@ detector.start(	detected_callback=detected_callback,
 
 
 detector.terminate()
-mqtt.publish("jarvis/hotword", "stopped")
--- engine/stt.py
+++ engine/stt.py
@@ -8,5 +8,47 @@
 ## input: jarvis/hotword -> detected
 ## output: jarvis/stt -> (:[words]|started|stopped|error)
 
+# stop reading input after this second count of silence
+END_OF_INPUT_AFTER_SECONDS = 2
 
-import lib.helper as helper
+import os
+import time
+from lib.helper import *
+from pocketsphinx import LiveSpeech
+
+
+def hotword_callback(client, userdata, message):
+	
+	message.payload = message.payload.decode()
+
+	print("mqtt received something")
+
+	log("mqtt", "received message '" + str(message.payload) + "' from topic '" + str(message.topic) + "'")
+
+	if message.payload == "detected":
+		log("stt", "hotword received")
+		log("stt", "start transcribing")
+
+		speech = LiveSpeech(
+			verbose=False,
+			sampling_rate=16000,
+			buffer_size=2048,
+			no_search=False,
+			full_utt=False,
+			audio_device=0,
+			hmm="/home/pi/jarvis/resources/stt/acoustic_model/",
+			lm="/home/pi/jarvis/resources/stt/german.lm.bin",
+			dic="/home/pi/jarvis/resources/stt/german.min.dict"
+		)
+
+		for phrase in speech:
+			print(phrase)
+
+
+
+mqtt = MQTT(client_id="jarvis.stt")
+mqtt.on_message(hotword_callback)
+mqtt.subscribe("jarvis/hotword")
+
+while True:
+	time.sleep(0.1)
